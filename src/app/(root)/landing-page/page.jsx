81d194534ffcbbe04335c050a3176b8098d2261f
Johntech-dev <johnayomide50@gmail.com>
2024-09-19 16:15:15 +0100

    desktop view for the navbar done

--- src/app/(root)/landing-page/page.jsx
+++ src/app/(root)/landing-page/page.jsx
@@ -1,10 +1,11 @@
+import HeroSection from '@/components/HeroSection'
 import Navbar from '@/components/Navbar'
 import React from 'react'
 
 const LandingPage = () => {
   return (
     <div>
-       <Navbar />
+       <HeroSection />
     </div>
   )
 }
